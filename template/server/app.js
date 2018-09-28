ab519cc064d3221f47e04397fed6f3c29f462818
yanweiqing <yanweiqing@meicai.cn>
2018-09-28 09:08:00 +0800

    重新添加template

---
+++ template/server/app.js
@@ -0,0 +1,12 @@
+// import MCServer from '@mc/fe-server';
+import MCServer from '../../index.js';  // 需要手动开启
+import config from 'config';
+
+import userDefined from './middlewares/user-defined.js';
+import routers from './middlewares/routers.js';
+
+MCServer(config)
+  .load(userDefined)
+  .loadDefault()
+  .load(routers.routes())
+  .start();
